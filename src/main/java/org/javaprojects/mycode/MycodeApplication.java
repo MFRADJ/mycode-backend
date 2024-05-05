d0867acb78fb0746f2e315291faf21ba2d315615
MFRADJ <mohamed-fradj@outlook.com>
2024-05-06 01:18:01 +0200

    first commit

---
+++ src/main/java/org/javaprojects/mycode/MycodeApplication.java
@@ -0,0 +1,13 @@
+package org.javaprojects.mycode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/javaprojects/mycode/MycodeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.javaprojects.mycode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
